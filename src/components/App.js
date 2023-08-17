df3760abba92afabc391c4d730668286c6c6ece6
obadia <obadia.maasai@student.moringaschool.com>
2023-08-17 12:59:05 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
+      
     </nav>
   );
 }
@@ -15,16 +16,23 @@ function Home() {
     </div>
   );
 }
+function About(){
+  return(
+    <div id="about"> 
+    <h2>About</h2>
+    </div>
+  )
+}
 
-{/* write an <About> component here */}
+// { About}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      < Home/>
+      <About/>
     </div>
   );
 }
